b1463d09e1e792914ba1c42b8f3b6958604eb056
shreyash chougule <shreyashchougule26@gmail.com>
2025-12-17 09:25:13 +0530

    day 3

--- day03/page.venv/page.py
+++ day03/page.venv/page.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # Load API key from .env
 load_dotenv()
-API_KEY = os.getenv("4893805586e199fdf05ac6ae16e5d413")
+API_KEY = os.getenv("apikey")
 
 st.title("Login → Weather App")
 
